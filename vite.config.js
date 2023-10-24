0174c76917d5f3ec89515a3577d6076e6077bc24
onkelarmin <97581983+onkelarmin@users.noreply.github.com>
2023-10-24 19:59:04 +1100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+import htmlPurge from 'vite-plugin-purgecss'
+
+export default defineConfig({
+  root: resolve(__dirname, 'src/'),
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        home: resolve(__dirname, 'src/index.html'),
+        // about: resolve(__dirname, 'src/about/index.html'),
+        // contact: resolve(__dirname, 'src/contact/index.html'),
+      },
+    },
+  },
+  plugins: [htmlPurge([htmlPurge()])],
+})
